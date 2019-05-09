dbe3ecf5e2b65fa4c80ad91e3a63607ae51a5f52
victor.monte <victorbcm@hotmail.com>
2019-05-08 21:46:17 -0300

    Add genate random phrase view

--- index.ios.js
+++ index.ios.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import { Text, AppRegistry, View } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity} from 'react-native';
 
 const Estilos = {
     principal: {
         flex: 1,
-        backgroundColor: 'cornflowerblue'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    topo: {
-        flex: 1,
-        backgroundColor: 'brown'
+    botao: {
+        backgroundColor: '#538530',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
     },
-    conteudo: {
-        flex: 8,
-        backgroundColor: 'yellowgreen'
-    },
-    rodape: {
-        flex: 1,
-        backgroundColor: 'orangered'
+    textoBotao: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 };
 
+const gerarNovaFrase = () => {
+    
+    var numeroAleatorio = Math.random();
+    numeroAleatorio = Math.floor(numeroAleatorio * 5);
+
+    var frases = [];
+    frases[0] = 'Frase 1';
+    frases[1] = 'Frase 2';
+    frases[2] = 'Frase 3';
+    frases[3] = 'Frase 4';
+    frases[4] = 'Frase 5';
+
+    var fraseEscolhida = frases[numeroAleatorio];
+
+    alert(fraseEscolhida);
+
+}
+
 const App = () => {
 
-    const { principal, topo, conteudo, rodape } = Estilos;
+    const { principal, botao, textoBotao} = Estilos;
 
     return (
         <View style={ principal }>
-            <Text style={ topo }>Topo</Text>
-            <Text style={ conteudo }>Conteudo</Text>
-            <Text style={ rodape }>Rodape</Text>
+            <Image source={require('./imgs/logo.png')} />
+            <TouchableOpacity style={botao} onPress={gerarNovaFrase}>
+                <Text style={textoBotao}>Nova frase</Text>
+            </TouchableOpacity>
         </View>
     );
 };
